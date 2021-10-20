21a63bcb0c6e3b4116e9df0287ec92489b9ae253
Davíð Guðni <davidgudni@gmail.com>
2021-10-20 09:22:55 +0200

    Use jest instead of mocha

--- test/calc.spec.ts
+++ test/calc.spec.ts
@@ -1,9 +1,8 @@
 import Calculator from '../lib/calc'
-import { expect } from 'chai'
 
 describe('Calculator', () => {
   it('should add two numbers together', () => {
     const calc: Calculator = new Calculator()
-    expect(calc.add(2, 3)).to.equal(5)
+    expect(calc.add(2, 3)).toBe(5)
   })
 })
